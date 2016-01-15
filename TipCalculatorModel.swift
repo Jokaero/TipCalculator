37f990cbd0a01930d3bcd988a0bb0544a122acd5
Markus Liechti <markus.liechti@local.ch>
2016-01-15 17:02:33 +0100

    Added TipCalculatorModel

---
+++ TipCalculatorModel.swift
@@ -0,0 +1,43 @@
+//
+//  TipCalculatorModel.swift
+//  TipCalculator
+//
+//  Created by Markus Liechti on 12.01.16.
+//  Copyright © 2016 Jokaero. All rights reserved.
+//
+
+import Foundation
+
+class TipCalculatorModel {
+  
+  var total: Double
+  var taxPct: Double
+  var subtotal: Double {
+    get {
+      return total / (taxPct + 1)
+    }
+  }
+  
+  init(total: Double, taxPct: Double) {
+    self.total = total
+    self.taxPct = taxPct
+  }
+  
+  func calcTipWithTipPct(tipPct: Double) -> Double {
+    return subtotal * tipPct
+  }
+  
+  func returnPossibleTips() -> [Int: Double] {
+    
+    let possibleTipsInferred = [0.15, 0.18, 0.20]
+    
+    var retval = [Int: Double]()
+    for possibleTip in possibleTipsInferred {
+      let intPct = Int(possibleTip*100)
+      retval[intPct] = calcTipWithTipPct(possibleTip)
+    }
+    return retval
+    
+  }
+  
+}
